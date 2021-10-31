b4a063d699ff893f446e41ffb0cfd5713ddfda14
QemerS <tu201806169@code.edu.az>
2021-11-01 02:11:10 +0400

    Add project files.

---
+++ TaskPST/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskPST
+{
+    class Person
+    {
+        public string Greet()
+        {
+            return "Hello!";
+        }
+        public int SetAge(int age)
+        {
+            return age;
+        }
+    }
+}
---
+++ TaskPST/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskPST
+{
+    class Student:Person
+    {
+        public string Study()
+        {
+            return "I'm studying";
+        }
+        public string ShowAge(byte x)
+        {
+            return $"My age is: {x} years old";
+        }
+    }
+}
